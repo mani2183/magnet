acd7aefd105154a60544b3aa1e837e791b51740a
mani2183 <mkmrmanishkumar4@gmail.com>
2026-04-30 12:33:19 +0530

    firts commit

---
+++ src/main/java/com/magnet/mann/MannApplication.java
@@ -0,0 +1,13 @@
+package com.magnet.mann;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MannApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/magnet/mann/MannApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magnet.mann;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MannApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
